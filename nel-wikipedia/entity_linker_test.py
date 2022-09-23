204e1019be1ee7aa4e08fcfeb9c0149c618f0fff
Vinay Setty <1608263+vinaysetty@users.noreply.github.com>
2022-09-23 14:10:32 +0200

    Adds text arg and uses spacy 2.3

--- nel-wikipedia/entity_linker_test.py
+++ nel-wikipedia/entity_linker_test.py
@@ -3,14 +3,21 @@ import spacy
 
 parser = argparse.ArgumentParser("Spacy entity linker example code")
 parser.add_argument(
-    "--model_path",
+    "--model-path",
     type=str,
     required=True,
     help="Path to the folder where the trained model for entity linking is present",
     default="spacy_nel_wikidata_wikipedia_en_train_output_sample_100/nlp"
 )
+parser.add_argument(
+    "--text",
+    type=str,
+    required=True,
+    help="Text to microfact",
+    default="The European Commission said on Thursday it disagreed with German advice to consumers to shun British lamb until scientists determine whether mad cow disease can be transmitted to sheep."
+)
 args = parser.parse_args()  
 nlp = spacy.load(args.model_path)
-doc = nlp("The European Commission said on Thursday it disagreed with German advice to consumers to shun British lamb until scientists determine whether mad cow disease can be transmitted to sheep.")
+doc = nlp(args.text)
 for ent in doc.ents:
     print(ent.text, ent.label_, ent.kb_id_)
